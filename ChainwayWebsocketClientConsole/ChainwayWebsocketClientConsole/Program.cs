3902643e2d90bfb241b33520895fc6d102e02393
fazaux <faza.aulia@tudi.id>
2024-04-22 07:20:56 -0700

    Hello world

---
+++ ChainwayWebsocketClientConsole/ChainwayWebsocketClientConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+
+var ws = new ClientWebSocket();
+
+Console.WriteLine("Connect to localhost:5000");
+await ws.ConnectAsync(new Uri("ws://localhost:5000"), CancellationToken.None);
+Console.WriteLine("Connected!");
+
+
+var receivedTask = Task.Run(async () =>
+{
+    var buffer = new byte[1024];
+    while (true)
+    {
+        var result = await ws.ReceiveAsync(buffer, CancellationToken.None);
+        
+        if(result.MessageType == WebSocketMessageType.Close)
+        {
+            break;
+        }
+
+        var message = Encoding.UTF8.GetString(buffer);
+        Console.WriteLine(message);
+    }
+});
+
+await receivedTask;
---
+++ ChainwayWebsocketServer/ChainwayWebsocketServer/Program.cs
@@ -0,0 +1,57 @@
+using System.Net.WebSockets;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<ChatService>();
+var app = builder.Build();
+app.UseWebSockets();
+
+Console.WriteLine("===== [ Running Chainway WebSocket server ] =====");
+
+app.MapGet("/", async (HttpContext context, ChatService chatService) =>
+{
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+        var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+        await chatService.HandleWebSocketConnection(webSocket);
+    }
+    else
+    {
+        context.Response.StatusCode = 400;
+        await context.Response.WriteAsync("Expected a WebSocket request");
+    }
+});
+
+app.Run();
+
+public class ChatService
+{
+    private readonly List<WebSocket> _sockets = new();
+
+    public async Task HandleWebSocketConnection(WebSocket socket)
+    {
+        _sockets.Add(socket);
+        var buffer = new byte[1024 * 2];
+        while (socket.State == WebSocketState.Open)
+        {
+            var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), default);
+            if (result.MessageType == WebSocketMessageType.Close)
+            {
+                await socket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, default);
+                break;
+            }
+
+            if (result.MessageType == WebSocketMessageType.Text)
+            {
+                var received_payload = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                Console.WriteLine(received_payload);
+            }
+
+            foreach (var s in _sockets)
+            {
+                await s.SendAsync(buffer[..result.Count], WebSocketMessageType.Text, true, default);
+            }
+        }
+        _sockets.Remove(socket);
+    }
+}
